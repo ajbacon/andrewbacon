541d376ffda6f12250234f1e004015b93299f48d
ajbacon <47368135+ajbacon@users.noreply.github.com>
2020-06-25 14:37:57 +0100

    feat: add social/contact links

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import './Home.css';
+import emailImg from './email-icon.png';
+import gitHubIcon from './github-icon.png';
 
 const Home = () => {
   return (
     <div className='home'>
       <div className='title'>Hi, I'm Andrew!</div>
+
+      <div className='contact-links'>
+        <a href='https://www.linkedin.com/in/andrew-bacon-a13942b3/'>
+          <img
+            className='contact-img'
+            src='https://www.iconfinder.com/data/icons/free-social-icons/67/linkedin_circle_color-512.png'
+            alt='linkedin-icon'
+          />
+        </a>
+        <a href='https://github.com/ajbacon'>
+          <img className='contact-img' src={gitHubIcon} alt='github-icon' />
+        </a>
+        <a href='mailto:abacon2013@gmail.com'>
+          <img className='contact-img' src={emailImg} alt='email-icon' />
+        </a>
+      </div>
       <div className='content'>
         <p>
           I'm a full-stack software engineer from London. Lorem Ipsum is simply
